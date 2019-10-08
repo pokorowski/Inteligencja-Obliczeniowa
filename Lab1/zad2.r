13c71e578b7f84318b663a6fb39ffb0f68f9c0dc
pokorowski <pawelokorowski@gmail.com>
2019-10-08 09:41:25 +0200

    Finished subsections a-f

--- Lab1/zad2.r
+++ Lab1/zad2.r
@@ -1,1 +1,11 @@
+getwd()
+osoby <- read.csv(file="osoby.csv", header=TRUE, sep=",")
+print(osoby)
+imiona = osoby[,c("imie")]
+print(imiona)
+kobiety <- subset( osoby, plec == "k")
+print(kobiety)
+mp50 <- subset(osoby, plec == "m" && wiek>50)
+write.csv(mp50,"osoby2.csv")
+mean <- mean(osoby$wiek)
 
