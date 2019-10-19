4ca4030bc57fe54a9eb3c8e06c38d3c06e5a3489
pokorowski <pawelokorowski@gmail.com>
2019-10-19 20:44:01 +0200

    Update zad3.py

--- zad3.py
+++ zad3.py
@@ -1,13 +1,19 @@
+import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
+x = np.random.randint(0, 21, size=100)
+y =np.array([0]*21)
+y_pos = list(range(0, 21))
+i = 0
 
+while i < len(x): 
+    y[x[i]] +=1
+    i+=1
 
-x = np.random.randint(1, 20, size=(1, 100))
-print(x)
-
-listx = a = [0] * 20
-
-
-for i in range(x):
-    listx[x[i]] +=1
+plt.bar(y_pos,y, align='center')
+plt.xticks(y_pos,y_pos)
+plt.ylabel('liczba wystapien')
+plt.xlabel('liczby')
+plt.title('rozklad 100 losowych liczb')
+plt.show()
