9d05afd236211884922299acb312a5e4d9a9d009
pokorowski <pawelokorowski@gmail.com>
2019-12-10 01:49:28 +0100

    Add files via upload

--- lab83.R
+++ lab83.R
@@ -54,8 +54,60 @@ prediction
 actual <- cbind(temp1[,10],temp1[,11])
 actual[,1] <- as.numeric(actual[,1])
 actual[,2] <- as.numeric(actual[,2]) 
+
+
+pr3 <- knn(iris_train,iris_test,cl=iris_target_category,k=3)
+pr5 <- knn(iris_train,iris_test,cl=iris_target_category,k=5)
+pr11 <- knn(iris_train,iris_test,cl=iris_target_category,k=11)
+
+bayes <- naiveBayes(iris_train, iris_target_category)
+
+myFormula <- class ~ .
+iris_ctree <- ctree(myFormula, data=iris[ran,])
+test_predict <- predict(iris_ctree,iris[-ran,],type="response")
+
+##create confusion matrix
+tab3 <- table(pr3,iris_test_category)
+tab5 <- table(pr5,iris_test_category)
+tab11 <- table(pr11,iris_test_category)
+tab_bayes <- table(predict(bayes, iris_test), iris_test_category)
+tab_tree <- table(test_predict,iris[-ran,]$class)
+
+##this function divides the correct predictions by total number of predictions that tell us how accurate teh model is.
+accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
+ac3 = accuracy(tab3)
+ac5 = accuracy(tab5)
+ac11 = accuracy(tab11)
+acb = accuracy(tab_bayes)
+act = accuracy(tab_tree)
+
 confusionMatrix <- table(actual,prediction)
-accuracy(confusionMatrix)
+acn = accuracy(confusionMatrix)
 confusionMatrix
+
+ac3
+tab3
+
+ac5
+tab5
+
+ac11
+tab11
+
+acb
+tab_bayes
+
+act
+tab_tree
+
+
+Accuracy <- c(ac3,ac5,ac11,acb,act,acn)
+Clasificator <- c("knn3","knn5","knn11","naive_bayes","decision_tree","Neuron")
+df <- data.frame(Clasificator,Accuracy)
+ggplot(data=df, aes(x=Clasificator, y=Accuracy)) +
+  geom_bar(stat="identity", fill="steelblue")+
+  geom_text(aes(label=Accuracy), vjust=1.6, color="white", size=3.5)+
+  theme_minimal()
+
 
 
